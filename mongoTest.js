c7aa9a7f53974c9ae7e7f87d13292abf62051e01
tyleralves <tyler11alves@gmail.com>
2015-12-16 15:50:46 -0800

    mongodb add

---
+++ mongoTest.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Tyler on 12/16/2015.
+ */
+var MongoClient = require('mongodb').MongoClient;
+// the client db connection scope is wrapped in a callback:
+MongoClient.connect('mongodb://'+connection_string, function(err, db) {
+    if(err) throw err;
+    var collection = db.collection('books').find().limit(10).toArray(function(err, docs) {
+        console.dir(docs);
+        db.close();
+    })
+});
